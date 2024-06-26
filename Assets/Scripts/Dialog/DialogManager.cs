ba49ceeac211a53fd603a136a3ed9be37fb5385b
IsabellThormann <isabell.thormann@hotmail.com>
2024-06-26 11:19:41 +0200

    mehr dialog fixes
    jetzt kann der npc antworten

--- Assets/Scripts/Dialog/DialogManager.cs
+++ Assets/Scripts/Dialog/DialogManager.cs
@@ -9,14 +9,12 @@ public class DialogManager : MonoBehaviour
     public GameObject dialogPanel; // Das Dialogfeld-UI-Element
     public TextMeshProUGUI dialogText; // Der Text-UI-Komponente für den Dialog
     public Image npcImage; // Referenz auf das Bildobjekt für den NPC
-    public Image npc0Image; // Referenz auf das Bildobjekt für den NPC
     public Image pcImage; // Referenz auf das Bildobjekt für den Player-Charakter
     public Button nextButton; // Der Button für die nächste Dialogzeile
 
-    private Queue<string> sentences; // Warteschlange für die Dialogsätze
-    public static bool isDialogActive = false; // Zustand des Dialogs //Neuer Kommentar
     private Queue<string> npcSentences; // Dialogsätze des NPCs
-    private Queue<string> pcSentences; // Dialogsätze des Player Characters (PC)
+    private string playerQuestion; // Dialogzeile des Players
+    private string npcResponse; // Antwort des NPCs auf die Frage des Spielers
     private bool isNpcSpeaking = true; // Flag, um zu verfolgen, wer spricht
 
     private NPC currentNpc; // Variable zum Speichern des aktuellen NPCs 
@@ -25,6 +23,7 @@ public class DialogManager : MonoBehaviour
     public TextMeshProUGUI npcNameText; // Text innerhalb des NPC Namensschildes
     public TextMeshProUGUI pcNameText; // Text innerhalb des PC Namensschildes
 
+    public static bool isDialogActive; // Diese Eigenschaft wurde hinzugefügt
 
     void Start()
     {
@@ -32,35 +31,32 @@ public class DialogManager : MonoBehaviour
         dialogPanel.SetActive(false); // Dialogfeld ausblenden
 
         npcSentences = new Queue<string>(); // Initialisiere die NPC-Dialog-Warteschlange
-        pcSentences = new Queue<string>(); // Initialisiere die PC-Dialog-Warteschlange
         nextButton.onClick.AddListener(DisplayNextSentence); // Button mit der Methode verknüpfen
     }
 
     public void StartDialog(NPC npc) // Diese Methode erwartet nur den NPC-Parameter
     {
-        currentNpc = npc; // Speichert den übergebenen NPC in der aktuellen Instanz //Neuer Kommentar
+        currentNpc = npc; // Speichert den übergebenen NPC in der aktuellen Instanz
         dialogPanel.SetActive(true); // Dialogfeld anzeigen
         npcSentences.Clear(); // Leert die NPC-Dialog-Warteschlange
-        pcSentences.Clear(); // Leert die PC-Dialog-Warteschlange
 
-        foreach (string sentence in npc.dialogLines)
+        foreach (string sentence in npc.initialDialogLines)
         {
             npcSentences.Enqueue(sentence); // Füge die NPC-Dialogzeilen zur Warteschlange hinzu
         }
 
-        foreach (string sentence in npc.pcDialogLines)
-        {
-            pcSentences.Enqueue(sentence); // Füge die PC-Dialogzeilen zur Warteschlange hinzu
-        }
+        playerQuestion = npc.playerQuestion; // Setze die Frage des Spielers
+        npcResponse = npc.npcResponse; // Setze die Antwort des NPCs auf die Frage
 
         isNpcSpeaking = true; // Starte mit NPC-Dialog
         npcImage.sprite = npc.npcSprite; // Setze das NPC-Image
         pcImage.sprite = npc.pcImage; // Setze das PC-Image (kann optional sein, je nach deiner Implementierung)
-        
+
         // Aktualisiere Namensschilder je nach Sprecher
         npcNameText.text = npc.npcName; // Setze den NPC-Namen
         pcNameText.text = "sexy chick"; // Setze den Namen des Spielers (kann je nach Bedarf variieren)
         
+        isDialogActive = true; // Setze den Dialogstatus auf aktiv
         DisplayNextSentence(); // Zeige die nächste Dialogzeile an
     }
 
@@ -70,7 +66,7 @@ public class DialogManager : MonoBehaviour
         {
             if (npcSentences.Count == 0)
             {
-                SwitchToPCDialog(); // Wechsel zu PC-Dialog
+                SwitchToPlayerQuestion(); // Wechsel zur Spielerfrage
                 return;
             }
 
@@ -87,18 +83,12 @@ public class DialogManager : MonoBehaviour
 
             Debug.Log("NPC Sprite gesetzt: " + (npcImage.sprite != null ? npcImage.sprite.name : "null")); // Debug-Ausgabe
         }
-        else
+        else if (playerQuestion != null)
         {
-            if (pcSentences.Count == 0)
-            {
-                EndDialog(); // Beende den Dialog
-                return;
-            }
-
-            string sentence = pcSentences.Dequeue(); // Nächste PC-Dialogzeile aus der Warteschlange
             StopAllCoroutines(); // Stoppe alle laufenden Coroutinen
-            StartCoroutine(TypeSentence(sentence)); // Starte die Coroutine zum schrittweisen Anzeigen des Satzes
-             // Zeige PC-Image an, verstecke NPC-Image
+            StartCoroutine(TypeSentence(playerQuestion)); // Starte die Coroutine zum schrittweisen Anzeigen der Frage
+            playerQuestion = null; // Leere die Spielerfrage nach dem Anzeigen
+            // Zeige PC-Image an, verstecke NPC-Image
             pcImage.gameObject.SetActive(true);
             npcImage.gameObject.SetActive(false);
 
@@ -108,28 +98,51 @@ public class DialogManager : MonoBehaviour
 
             Debug.Log("PC Image sichtbar: " + pcImage.gameObject.activeSelf); // Debug-Ausgabe
         }
+        else
+        {
+            if (npcResponse != null)
+            {
+                StopAllCoroutines(); // Stoppe alle laufenden Coroutinen
+                StartCoroutine(TypeSentence(npcResponse)); // Starte die Coroutine zum schrittweisen Anzeigen der Antwort
+                npcResponse = null; // Leere die NPC-Antwort nach dem Anzeigen
+                // Zeige NPC-Image an, verstecke PC-Image
+                npcImage.gameObject.SetActive(true);
+                pcImage.gameObject.SetActive(false);
+
+                // Aktiviere das NPC Namensschild und deaktiviere das PC Namensschild
+                npcNamePanel.SetActive(true);
+                pcNamePanel.SetActive(false);
+
+                Debug.Log("NPC Sprite gesetzt: " + (npcImage.sprite != null ? npcImage.sprite.name : "null")); // Debug-Ausgabe
+            }
+            else
+            {
+                EndDialog(); // Beende den Dialog
+            }
+        }
     }
 
     IEnumerator TypeSentence(string sentence)
     {
-        dialogText.text = ""; // Setze den Dialogtext zurück
+        dialogText.text = ""; // Setze den Dialogtext auf leer
         foreach (char letter in sentence.ToCharArray())
         {
-            dialogText.text += letter; // Füge den nächsten Buchstaben zum Dialogtext hinzu
+            dialogText.text += letter; // Füge Buchstaben nacheinander hinzu
             yield return null; // Warte einen Frame
         }
     }
 
-    void SwitchToPCDialog()
+    void SwitchToPlayerQuestion()
     {
-        isNpcSpeaking = false; // Starte mit PC-Dialog
-        DisplayNextSentence(); // Zeige die nächste Dialogzeile an
+        isNpcSpeaking = false; // Wechsle zu Spielerfrage
+        DisplayNextSentence(); // Zeige die Spielerfrage an
     }
 
     void EndDialog()
     {
-        dialogPanel.SetActive(false); // Dialogfeld ausblenden
-        isDialogActive = false; // Dialogzustand deaktivieren //Neuer Kommentar
-        // Weitere Logik nach dem Dialogende, falls nötig
+        dialogPanel.SetActive(false); // Blende das Dialogfeld aus
+        isNpcSpeaking = true; // Setze den Sprecher auf NPC zurück
+        isDialogActive = false; // Setze den Dialogstatus auf inaktiv
+        currentNpc.OnDialogEnd(); // Rufe die Methode OnDialogEnd des aktuellen NPCs auf
     }
 }
--- Assets/Scripts/Dialog/NPCBartender.cs
+++ Assets/Scripts/Dialog/NPCBartender.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    public string[] dialogLines; // Array der Dialogzeilen für den NPC
+    public string[] initialDialogLines; // Array der Dialogzeilen für den NPC
+    public string playerQuestion; // Dialogzeile für die Frage des Spielers
+    public string npcResponse; // Antwort des NPCs auf die Frage des Spielers
     public ItemData requiredItem; // Das benötigte Item, um den Dialog zu starten
     public ItemData rewardItem; // Das Item, das der NPC nach Erfüllung der Bedingung gibt
 
     private bool hasReceivedItem = false; // Gibt an, ob der NPC das benötigte Item erhalten hat
     public Sprite npcSprite; // Das Bild des NPCs
     public Sprite pcImage; // Das Bild des Player Characters (PC)
-    public string[] pcDialogLines; // Array der Dialogzeilen für den Player Character (PC)
 
     // Neues Feld für den NPC-Namen
     public string npcName;
@@ -29,21 +30,23 @@ public class NPC : MonoBehaviour
 
                 GameManager.collectedItems.Add(rewardItem); // Füge das Belohnungs-Item zur Sammlung hinzu
 
-                dialogLines = new string[] { "Wow du super hecht", "hier mein super shaker" }; // Setze die Dialogzeilen des NPCs
-                pcDialogLines = new string[] { "ich bin eine Frau...", "..Abstand" }; // Setze die Dialogzeilen des PCs
+                initialDialogLines = new string[] { "Wow du super hecht", "hier mein super shaker" }; // Setze die Dialogzeilen des NPCs
+                playerQuestion = "ich bin eine Frau...";
+                npcResponse = "..Abstand";
             }
             else
             {
-                dialogLines = new string[] { "O nourrrrrrr, I can’t do this anymore.", " My poor snakey Bertha…", "...she’s gone...", "I can’t find her.",  "If someone brings Bertha back to me, I would let them use my special cocktail shaker." }; // Setze die Dialogzeilen des NPCs
-                pcDialogLines = new string[] { "hmmmmm" }; // Setze die Dialogzeilen des PCs
+                initialDialogLines = new string[] { "O nourrrrrrr, I can’t do this anymore.", " My poor snakey Bertha…", "...she’s gone...", "I can’t find her.",  "If someone brings Bertha back to me, I would let them use my special cocktail shaker." }; // Setze die Dialogzeilen des NPCs
+                playerQuestion = "hmmmmm";
+                npcResponse = "";
             }
         }
         else
         {
             Debug.Log("NPC hat das benötigte Item bereits erhalten oder benötigt kein Item"); // Debug-Ausgabe für den Zustand des NPCs
-            dialogLines = new string[] { "Oh my, you scared me Bertha!!Where were you?", "Thank you, total stranger, that I’ve never seen before....", " Here, now you can make special drinks just like me. You can’t take it with you, just bring me 2 ingredients and I can mix it up for ya."  }; // Setze die Dialogzeilen des NPCs
-            pcDialogLines = new string[] { "Do you know, how to get the key to the wedding chapel? " }; // Setze die Dialogzeilen des PCs
-            dialogLines = new string[] { "Well, I knaurr that the chapel keys are kept by the security in the entrance." };
+            initialDialogLines = new string[] { "Oh my, you scared me Bertha!!Where were you?", "Thank you, total stranger, that I’ve never seen before....", " Here, now you can make special drinks just like me. You can’t take it with you, just bring me 2 ingredients and I can mix it up for ya." }; // Setze die Dialogzeilen des NPCs
+            playerQuestion = "Do you know, how to get the key to the wedding chapel?";
+            npcResponse = "Well, I knaurr that the chapel keys are kept by the security in the entrance.";
         }
 
         Debug.Log("NPC Sprite gesetzt: " + (npcSprite != null ? npcSprite.name : "null")); // Debug-Ausgabe für das NPC-Bild
