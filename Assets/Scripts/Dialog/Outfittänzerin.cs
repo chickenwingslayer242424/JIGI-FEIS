ab7c929607f4534831b84c83974c3b755c7c76c5
IsabellThormann <isabell.thormann@hotmail.com>
2024-07-09 21:13:59 +0200

    mehr npcs hehe

---
+++ Assets/Scripts/Dialog/Outfittänzerin.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutfitTänzerin : NPC
+{
+    public override void Interact()
+    {
+        Debug.Log("tänzer Interact aufgerufen");
+
+        // Setze die Dialogzeilen für den KeySpawner
+        initialDialogLines = new string[] {"Ugh, it's so frustrating.",  "I can't even start my routine properly because the music keeps skipping.", "The missing lyrics are throwing off my entire performance." };
+        playerQuestions1 = new string[] {"Maybe you could find something to help you fill in the missing lyrics." ,"That might get the music flowing right." };
+        npcResponses1 = new string[] { "If you can make that happen, honey", "I'll tear these bell-bottoms right off and give them to you as a souvenir.", "Deal?"};
+        // Starte den Dialog direkt ohne die Interact-Methode der Basisklasse aufzurufen
+        FindObjectOfType<DialogManager>().StartDialog(this);
+
+        Debug.Log("Singer Sprite gesetzt: " + (npcSprite != null ? npcSprite.name : "null"));
+    }
+
+    public override void OnDialogEnd()
+    {
+        Debug.Log("Singer mit Granny beendet");
+    }
+}
